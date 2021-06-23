826132d349ba5ad2a39d7f63104d5dd798cf6433
“BIT <ivanaidraza@gmail.com>
2021-06-23 18:05:37 +0200

    refactored

--- src/pages/SingleCandidate/SingleCandidate.js
+++ src/pages/SingleCandidate/SingleCandidate.js
@@ -1,15 +1,17 @@
 import "./SingleCandidate.scss";
 import Header from "../../components/Header/Header";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 const SingleCandidate = ({ candidates, companies, reports, match }) => {
-  
+  const [candidate, setCandidate] = useState(null);
 
-  const filterCandidate = candidates?.filter((e) => e.id == match.params.id);
-  console.log(filterCandidate);
   const filterReports = reports?.filter(
     (e) => e.candidateId == match.params.id
   );
-  console.log(filterReports);
+
+  useEffect(() => {
+      const filtered = candidates?.filter((e) => e.id == match.params.id);
+      setCandidate(filtered[0])
+  }, [candidates])
 
   return (
     <div className="SingleCandidate">
@@ -17,10 +19,10 @@ const SingleCandidate = ({ candidates, companies, reports, match }) => {
       <div className="InterviewReport">
         <div className="person-card">
           <img src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"></img>
-          <h3></h3>
-          <p></p>
-          <p></p>
-          <p></p>
+          <h3>{candidate?.name}</h3>
+          <p>{filterCandidate.birthday}</p>
+          <p>{filterCandidate.email}</p>
+          <p>{filterCandidate.education}</p>
         </div>
         <div className="person-reports">
           <h2>Reports</h2>
@@ -30,6 +32,22 @@ const SingleCandidate = ({ candidates, companies, reports, match }) => {
               <th>Interview Date</th>
               <th>Status</th>
             </tr>
+            {filterReports.map((e) => {
+              return (
+                <tr>
+                  <td>{e.companyName}</td>
+                  <td>{e.interviewDate}</td>
+                  <td>{e.status}</td>
+                  <td
+                  // onClick={() => {
+                  //   setModular(e.id);
+                  // }}
+                  >
+                    More
+                  </td>
+                </tr>
+              );
+            })}
           </table>
         </div>
         <div className="modular">
